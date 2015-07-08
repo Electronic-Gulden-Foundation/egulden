c584fe6177a1bb0d11cba983620d62d8b3caca4b
Dennis Ruhe <dennis@amazingsystems.nl>
2015-07-08 10:54:42 +0200

    Fixed double ifndef/ifdef

--- src/util.h
+++ src/util.h
@@ -97,7 +97,6 @@ const boost::filesystem::path &GetDataDir(bool fNetSpecific = true);
 boost::filesystem::path GetConfigFile();
 #ifndef WIN32
 boost::filesystem::path GetPidFile();
-#ifndef WIN32
 void CreatePidFile(const boost::filesystem::path &path, pid_t pid);
 #endif
 void ReadConfigFile(std::map<std::string, std::string>& mapSettingsRet, std::map<std::string, std::vector<std::string> >& mapMultiSettingsRet);
