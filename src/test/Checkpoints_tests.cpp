2d7677d78c4dfc06c18d2188b565231343ef9c63
Dennis Ruhe <dennis@amazingsystems.nl>
2015-08-10 10:06:31 +0200

    Fixed checkpoint tests

--- src/test/Checkpoints_tests.cpp
+++ src/test/Checkpoints_tests.cpp
@@ -18,21 +18,21 @@ BOOST_AUTO_TEST_SUITE(Checkpoints_tests)
 
 BOOST_AUTO_TEST_CASE(sanity)
 {
-    uint256 p1500 = uint256("0x841a2965955dd288cfa707a755d05a54e45f8bd476835ec9af4402a2b59a2967");
-    uint256 p120000 = uint256("0xbd9d26924f05f6daa7f0155f32828ec89e8e29cee9e7121b026a7a3552ac6131");
-    BOOST_CHECK(Checkpoints::CheckBlock(1500, p1500));
-    BOOST_CHECK(Checkpoints::CheckBlock(120000, p120000));
+    uint256 p25000 = uint256("0x99ffd2f7230224586c61deb459fa1cde94bda2876aed8bb791f4e3b5d485ef59");
+    uint256 p40000 = uint256("0xbec732146705189352bb0007c4a26345a3ddf0eac1e8721e2fbb121b7d7c04b6");
+    BOOST_CHECK(Checkpoints::CheckBlock(25000, p25000));
+    BOOST_CHECK(Checkpoints::CheckBlock(40000, p40000));
 
-    
+
     // Wrong hashes at checkpoints should fail:
-    BOOST_CHECK(!Checkpoints::CheckBlock(1500, p120000));
-    BOOST_CHECK(!Checkpoints::CheckBlock(120000, p1500));
+    BOOST_CHECK(!Checkpoints::CheckBlock(25000, p40000));
+    BOOST_CHECK(!Checkpoints::CheckBlock(40000, p25000));
 
     // ... but any hash not at a checkpoint should succeed:
-    BOOST_CHECK(Checkpoints::CheckBlock(1500+1, p120000));
-    BOOST_CHECK(Checkpoints::CheckBlock(120000+1, p1500));
+    BOOST_CHECK(Checkpoints::CheckBlock(25000+1, p40000));
+    BOOST_CHECK(Checkpoints::CheckBlock(40000+1, p25000));
 
-    BOOST_CHECK(Checkpoints::GetTotalBlocksEstimate() >= 120000);
-}    
+    BOOST_CHECK(Checkpoints::GetTotalBlocksEstimate() >= 300000);
+}
 
 BOOST_AUTO_TEST_SUITE_END()
