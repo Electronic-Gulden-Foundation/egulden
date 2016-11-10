25bf96c88348c95bd0929f01b8456498dc549c55
Dennis Ruhe <dennis@amazingsystems.nl>
2016-11-10 10:13:54 +0100

    Fixed rpc tests

--- src/test/rpc_tests.cpp
+++ src/test/rpc_tests.cpp
@@ -134,8 +134,8 @@ BOOST_AUTO_TEST_CASE(rpc_parse_monetary_values)
     BOOST_CHECK_EQUAL(AmountFromValue(ValueFromString("0.50000000")), 50000000LL);
     BOOST_CHECK_EQUAL(AmountFromValue(ValueFromString("0.89898989")), 89898989LL);
     BOOST_CHECK_EQUAL(AmountFromValue(ValueFromString("1.00000000")), 100000000LL);
-    BOOST_CHECK_EQUAL(AmountFromValue(ValueFromString("20999999.9999999")), 2099999999999990LL);
-    BOOST_CHECK_EQUAL(AmountFromValue(ValueFromString("20999999.99999999")), 2099999999999999LL);
+    BOOST_CHECK_EQUAL(AmountFromValue(ValueFromString("2099999.9999999")), 209999999999990LL);
+    BOOST_CHECK_EQUAL(AmountFromValue(ValueFromString("2099999.99999999")), 209999999999999LL);
 }
 
 BOOST_AUTO_TEST_CASE(json_parse_errors)
