939b814ab11276ffa4fe1fc681f1a13f9623dd46
Dennis Ruhe <dennis@amazingsystems.nl>
2017-08-29 17:17:30 +0200

    CI

--- contrib/gitlab-ci/install-builddeps.sh
+++ contrib/gitlab-ci/install-builddeps.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+OLD_PWD=$PWD
+TMP=/tmp
+
 apt-get update
 apt-get dist-upgrade -y
 apt-get install -y \
@@ -29,6 +32,7 @@ apt-get install -y \
     wget
 
 # Build libdb4.8
+cd $TMP
 mkdir -p $BDB_PREFIX
 wget 'http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz'
 echo '12edc0df75bf9abd7f82f821795bcee50f42cb2e5f76a6a281b85732798364ef  db-4.8.30.NC.tar.gz' | sha256sum -c
@@ -36,3 +40,16 @@ tar -xzvf db-4.8.30.NC.tar.gz
 cd db-4.8.30.NC/build_unix/
 ../dist/configure --enable-cxx --disable-shared --with-pic --prefix=$BDB_PREFIX
 make install
+
+# Build libboost
+cd $TMP
+mkdir -p $BOOST_PREFIX
+wget 'http://dl.bintray.com/boostorg/release/1.65.0/source/boost_1_65_0.tar.gz'
+echo '8a142d33ab4b4ed0de3abea3280ae3b2ce91c48c09478518c73e5dd2ba8f20aa  boost_1_65_0.tar.gz' | sha256sum -c
+tar -xzvf boost_1_65_0.tar.gz
+cd boost_1_65_0
+./bootstrap.sh --prefix=$BOOST_PREFIX
+./b2 install
+
+# Move back to where the script was called from
+cd $OLD_PWD
