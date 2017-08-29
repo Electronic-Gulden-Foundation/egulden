fa05bb0397798a1c471e72130b068d9a564b3b32
Dennis Ruhe <dennis@amazingsystems.nl>
2017-08-29 11:39:08 +0200

    Fixed subsidy tests

--- src/test/main_tests.cpp
+++ src/test/main_tests.cpp
@@ -15,7 +15,7 @@ BOOST_FIXTURE_TEST_SUITE(main_tests, TestingSetup)
 static void TestBlockSubsidyHalvings(const Consensus::Params& consensusParams)
 {
     int maxHalvings = 64;
-    CAmount nInitialSubsidy = 50 * COIN;
+    CAmount nInitialSubsidy = 25 * COIN;
 
     CAmount nPreviousSubsidy = nInitialSubsidy * 2; // for height == 0
     BOOST_CHECK_EQUAL(nPreviousSubsidy, nInitialSubsidy * 2);
@@ -49,11 +49,15 @@ BOOST_AUTO_TEST_CASE(subsidy_limit_test)
     CAmount nSum = 0;
     for (int nHeight = 0; nHeight < 56000000; nHeight += 1000) {
         CAmount nSubsidy = GetBlockSubsidy(nHeight, consensusParams);
-        BOOST_CHECK(nSubsidy <= 50 * COIN);
+        if (nHeight == 1) {
+            BOOST_CHECK_EQUAL(nSubsidy, 10500000 * COIN);
+        } else {
+            BOOST_CHECK(nSubsidy <= 25 * COIN);
+        }
         nSum += nSubsidy * 1000;
         BOOST_CHECK(MoneyRange(nSum));
     }
-    BOOST_CHECK_EQUAL(nSum, 8399999990760000ULL);
+    BOOST_CHECK_EQUAL(nSum, 1049999997690000ULL);
 }
 
 bool ReturnFalse() { return false; }
