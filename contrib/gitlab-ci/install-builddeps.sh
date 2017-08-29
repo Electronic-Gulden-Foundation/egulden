5ba964be0ef1f1fb9c0add39ab58eb3072af422e
Dennis Ruhe <dennis@amazingsystems.nl>
2017-08-29 17:29:00 +0200

    Using gitlab build cache

--- contrib/gitlab-ci/install-builddeps.sh
+++ contrib/gitlab-ci/install-builddeps.sh
@@ -32,24 +32,35 @@ apt-get install -y \
     wget
 
 # Build libdb4.8
-cd $TMP
-mkdir -p $BDB_PREFIX
-wget 'http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz'
-echo '12edc0df75bf9abd7f82f821795bcee50f42cb2e5f76a6a281b85732798364ef  db-4.8.30.NC.tar.gz' | sha256sum -c
-tar -xzvf db-4.8.30.NC.tar.gz
-cd db-4.8.30.NC/build_unix/
-../dist/configure --enable-cxx --disable-shared --with-pic --prefix=$BDB_PREFIX
-make install
+
+if [[ -d $BDB_PREFIX ]]
+then
+    echo "$BDB_PREFIX exists -- asuming cached"
+else
+    cd $TMP
+    mkdir -p $BDB_PREFIX
+    wget 'http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz'
+    echo '12edc0df75bf9abd7f82f821795bcee50f42cb2e5f76a6a281b85732798364ef  db-4.8.30.NC.tar.gz' | sha256sum -c
+    tar -xzvf db-4.8.30.NC.tar.gz
+    cd db-4.8.30.NC/build_unix/
+    ../dist/configure --enable-cxx --disable-shared --with-pic --prefix=$BDB_PREFIX
+    make install
+fi
 
 # Build libboost
-cd $TMP
-mkdir -p $BOOST_PREFIX
-wget 'http://dl.bintray.com/boostorg/release/1.65.0/source/boost_1_65_0.tar.gz'
-echo '8a142d33ab4b4ed0de3abea3280ae3b2ce91c48c09478518c73e5dd2ba8f20aa  boost_1_65_0.tar.gz' | sha256sum -c
-tar -xzvf boost_1_65_0.tar.gz
-cd boost_1_65_0
-./bootstrap.sh --prefix=$BOOST_PREFIX
-./b2 install
+if [[ -d $BOOST_PREFIX ]]
+then
+    echo "$BOOST_PREFIX exists -- assuming cached"
+else
+    cd $TMP
+    mkdir -p $BOOST_PREFIX
+    wget 'http://dl.bintray.com/boostorg/release/1.65.0/source/boost_1_65_0.tar.gz'
+    echo '8a142d33ab4b4ed0de3abea3280ae3b2ce91c48c09478518c73e5dd2ba8f20aa  boost_1_65_0.tar.gz' | sha256sum -c
+    tar -xzvf boost_1_65_0.tar.gz
+    cd boost_1_65_0
+    ./bootstrap.sh --prefix=$BOOST_PREFIX
+    ./b2 install
+fi
 
 # Move back to where the script was called from
 cd $OLD_PWD
