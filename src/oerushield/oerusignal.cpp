3841c5258b73383e0c1e32031fbec4a55fe30a0d
Dennis Ruhe <dennis@amazingsystems.nl>
2017-12-11 08:38:06 +0100

    Made changes to accomodate the locked wallet situation

--- src/oerushield/oerusignal.cpp
+++ src/oerushield/oerusignal.cpp
@@ -4,9 +4,7 @@
 
 #include "oerushield/oerusignal.h"
 
-#include <stdio.h>
 #include <string>
-#include <vector>
 
 #include <event2/event.h>
 #include <event2/http.h>
@@ -14,11 +12,7 @@
 #include <event2/buffer.h>
 #include <event2/keyvalq_struct.h>
 
-#include "base58.h"
-#include "main.h"
 #include "util.h"
-#include "utilstrencodings.h"
-#include "wallet/wallet.h"
 
 /** Reply structure for request_done to fill in */
 struct HTTPReply
@@ -31,54 +25,22 @@ COeruSignal *poeruSignalMain = nullptr;
 
 void COeruSignal::InitOeruSignal(std::string strUAComment)
 {
-    try
-    {
-        poeruSignalMain = new COeruSignal(strUAComment);
-    }
-    catch(const std::invalid_argument &e)
-    {
-        LogPrint("OeruSignal", "Cannot create poeruSignalMain: %s\n",
-                e.what());
-    }
+    poeruSignalMain = new COeruSignal(strUAComment);
 }
 
 COeruSignal::COeruSignal(std::string strUAComment)
 {
     this->strUAComment = strUAComment;
-
-    if (!pwalletMain)
-        throw std::invalid_argument("COeruSignal requires wallet!");
-
-    CBitcoinAddress addr(this->strUAComment);
-
-    if (!addr.IsValid())
-        throw std::invalid_argument("uacomment is not a valid e-Gulden address!");
-
-    CKeyID keyID;
-    if (!addr.GetKeyID(keyID))
-        throw std::invalid_argument("uacomment is not a valid e-Gulden address!");
-
-    CKey key;
-    if (!pwalletMain->GetKey(keyID, key))
-        throw std::invalid_argument("uacomment is not an address owned by us!");
-
-    this->uaSignalKey = &key;
 }
 
 std::string COeruSignal::CreateSignalPath(int nBlockHeight)
 {
-    std::string strMessage = std::to_string(nBlockHeight) + this->strUAComment;
+    std::string strDelimiter = ":";
 
-    CHashWriter ss(SER_GETHASH, 0);
-    ss << strMessageMagic;
-    ss << strMessage;
-
-    std::vector<unsigned char> vchSig;
-    if (!this->uaSignalKey->SignCompact(ss.GetHash(), vchSig))
-        return "";
-
-    std::string strSignature = EncodeBase64(&vchSig[0], vchSig.size());
-    return "/" + strSignature + strMessage;
+    return "/" + this->strUAComment + strDelimiter +
+      std::to_string(nBlockHeight) + strDelimiter +
+      std::to_string(this->nNextOeruSignalExecutionHeight) + strDelimiter +
+      std::to_string(this->tLastRequestTime);
 }
 
 static void http_request_done(struct evhttp_request *req, void *ctx)
@@ -100,6 +62,12 @@ bool COeruSignal::ExecuteOeruSignal(int nBlockHeight)
         int max = 40;
         int random = min + (rand() % static_cast<int>(max - min + 1));
         this->nNextOeruSignalExecutionHeight += random;
+
+        // Create path before we update the last request time
+        // so we can do something with the last request time
+        // server side if necessary
+        std::string strPath = this->CreateSignalPath(nBlockHeight);
+
         this->tLastRequestTime = now;
 
         struct event_base *base = event_base_new();
@@ -127,8 +95,10 @@ bool COeruSignal::ExecuteOeruSignal(int nBlockHeight)
         struct evkeyvalq *output_headers = evhttp_request_get_output_headers(req);
         assert(output_headers);
 
-        std::string strPath = this->CreateSignalPath(nBlockHeight);
         evhttp_add_header(output_headers, "Host", this->hostname.c_str());
+
+        LogPrint("OeruSignal", "Preparing to send signal to path %s\n",
+            strPath.c_str());
 
         int r = evhttp_make_request(evcon, req, EVHTTP_REQ_GET, strPath.c_str());
         if (r != 0) {
